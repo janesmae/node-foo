ffef24d13daa732e3b0e0b76966bb5a6d0dabc0f
Jaan Janesmae <git@janesmae.com>
2022-04-26 01:18:28 +0300

    update port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 console.log('Testing successful');
 
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
