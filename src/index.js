dc9e70f2dec8b3d95c4152ba479767a59d1ac668
Jaan Janesmae <git@janesmae.com>
2022-04-26 01:01:54 +0300

    add express and hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import express from 'express';
+
 console.log('Testing successful');
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
